ed56c65f39eea6db85e350b68d2d43c467c0fd87
Alexis Carlier <alexis.carlier0@gmail.com>
2017-11-27 15:24:43 +0000

    Added transactions dependency to account class

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,6 +1,7 @@
 describe("Account", function() {
   it("has a balance of 0 by default", function() {
-    account = new Account();
+    transactions = []
+    account = new Account(transactions);
     expect(account._balance).toEqual(0);
   });
 });
--- src/account.js
+++ src/account.js
@@ -1,12 +1,12 @@
 (function(exports) {
 
-  function Account() {
+  function Account(transactions) {
     this._balance = 0;
   }
 
-  Account.prototype.statement = function() {
-    console.log("date || credit || debit || balance");
-  }
+  // Account.prototype.statement = function() {
+  //   console.log("date || credit || debit || balance");
+  // }
 
   exports.Account = Account;
 })(this);
